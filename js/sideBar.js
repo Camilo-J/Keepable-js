4074b51395cbc406b4dde1190830ddd819e04bff
Camilo-J <josecamilo1902@outlook.com>
2023-02-12 20:40:36 -0500

    Add listeners to sideBar

--- js/sideBar.js
+++ js/sideBar.js
@@ -1,13 +1,15 @@
+import STORE from "../store.js";
+
 function SideBar() {
   const template = `
     <section class="lateral py-1 ">
-    <a class="lateral__links" href="index.html">
+    <a class="lateral__links" href="index.html" id="homePage">
       <svg>
         <use xlink:href="#keys" />
       </svg>
       Notes
     </a>
-    <a href="#" class="lateral__links">
+    <a href="#" class="lateral__links" id="trashPage">
       <svg>
         <use xlink:href="#trashSidebar" />
       </svg>
@@ -15,12 +17,24 @@ function SideBar() {
     </a>
   </section>
     `;
-
+  function goListener() {
+    const links = document.querySelectorAll(".lateral__links");
+    links.forEach((link) => {
+      link.addEventListener("click", () => {
+        // STORE
+        console.log("funcionando");
+        console.log(link.id);
+        STORE.changeCurrentPage(link.id);
+      });
+    });
+  }
   return {
     toString() {
       return template;
     },
-    addListeners() {},
+    addListeners() {
+      goListener();
+    },
   };
 }
 
